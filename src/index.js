82dc3a50b0a16056c3b80f178b574442a24e0be2
Varghesemartin <71536249+Varghesemartin@users.noreply.github.com>
2022-07-15 20:14:06 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import Signup from './signup.js'
+import "./Signup.css";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Signup/>);
+// process.env.CI = false;
---
+++ src/signup.js
@@ -0,0 +1,89 @@
+import Logo from './Logo.svg'
+import {Helmet} from 'react-helmet';
+import {Magic} from 'magic-sdk';
+import {useForm} from "react-hook-form";
+//import login from "./login";
+//import axios from "axios";
+import './Signup.css'
+
+
+function Signup() {
+    //const magic = new Magic('pk_live_AABC93231E65FC6F');
+    const {register, handleSubmit} = useForm();
+    const onSubmit = data => {console.log(data);
+        // const did_Token = await magic.auth.loginWithMagicLink({email})
+        // console.log(did_Token)
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+                Origin: "https://sics-fast.herokuapp.com/"
+            },
+            body:JSON.stringify(data)
+
+        };
+
+        fetch('https://cors-anywhere.herokuapp.com/https://sics-fast.herokuapp.com/account/signup', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+
+
+
+    };
+    return (
+
+        <div className="container">
+            <Helmet>
+                <title>SICS | Sign Up</title>
+                <link rel="icon" href="Logo.svg" type="image/svg"/>
+            </Helmet>
+            <img className="Logo" src={Logo} id={Logo} width="150" height="120"/><br/><br/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>SignUp to continue to</label><br/><label> SICS Dashboard</label><br/>
+                <input className="FirstName" type="text" placeholder="FirstName" {...register("first_name")}
+                       required/><br></br>
+
+                <input className="LastName" type="text" placeholder="LastName"  {...register("last_name")}
+                       required/><br></br>
+
+                <input className="Email" type="email" placeholder="Email address"  {...register("email")}
+                       required/><br></br>
+
+                <input className="CompanyName" type="text" placeholder="Company Name"  {...register("company_name")}
+                       required/><br></br><br></br>
+
+                <button className="SignUp" type="submit">Sign Up</button>
+                <br></br>
+
+
+
+
+            </form>
+
+        </div>
+
+
+
+
+    )
+
+}
+
+export default Signup;
+
+
+
+// fetch("https://sics-python.herokuapp.com/",{
+//     mode: 'no-cors',
+//     method: 'GET',
+//     header: {
+//         authorization: "Bearer " + did_Token,
+//         "Content-type": 'application/json',
+//         "accept": 'application/json'
+//     },
+// }).then(async  (resp) => {
+//     const response = await resp.json();
+//     console.log(response)
+// })
